26ef2b40daa4dffe0b3c281b01d6e47f9262c751
orpere <orlando.capoeiraraiz@gmail.com>
2019-12-05 08:57:44 +0100

    json

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ module "consul" {
   source  = "hashicorp/consul/aws"
   version = "0.7.3"
   # insert the 4 required variables here
-  ami_id = var.ami_id
+  ami_id       = var.ami_id
   ssh_key_name = var.ssh_key_name
-  vpc_id = var.vpc_id
-  spot_price = var.spot_price
+  vpc_id       = var.vpc_id
+  spot_price   = var.spot_price
 }
 
 module "vault" {
@@ -18,8 +18,8 @@ module "vault" {
   version = "0.13.3"
   # insert the 4 required variables here
   hosted_zone_domain_name = var.hosted_zone_domain_name
-  ami_id = var.v_ami_id
-  vault_domain_name = var.vault_domain_name
-  ssh_key_name = var.ssh_key_name
+  ami_id                  = var.v_ami_id
+  vault_domain_name       = var.vault_domain_name
+  ssh_key_name            = var.ssh_key_name
 }
 
